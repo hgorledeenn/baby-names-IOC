71287d674b15387037519362186dec2bac5d6870
Holden Green <83799481+hgorledeenn@users.noreply.github.com>
2026-02-04 20:35:41 -0500

    update

--- BABY NAMES/baby_names.R
+++ BABY NAMES/baby_names.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-df = read_csv("~/Downloads/Popular_Baby_Names_20260203.csv")
+df = read_csv("~/Desktop/CJS/0126algorithms/HW-in-one-chart-1/BABY NAMES/Popular_Baby_Names_20260203.csv")
 
 df_no_yrs <- df %>%
   mutate(name = str_to_title(`Child's First Name`)) %>%
@@ -18,14 +18,47 @@ df_only_mandf <- df_wider %>%
   arrange(desc(pct_diff))
 
 
-df_only_mandf %>%
+vertical_cols <- df_only_mandf %>%
   ggplot() +
   aes(x=reorder(name, -pct_diff), y=pct_diff, fill=diff_size) +
   geom_col() +
   labs(
     title="% Difference in occurrences of names between females and males",
-    subtitle = str_wrap("Positive values show a name is more common among women, negative show a name is more common among men.", width=65)
+    subtitle = str_wrap("Positive values show a name is more common among women, negative show a name is more common among men.", width=65),
+    x = "Names",
+    y = "Percent difference in occurrences of name by gender",
+    fill = str_wrap("Total Name Count", width=15)
   ) +
   theme(
     axis.text.x = element_text(size = 10, angle=270)
     )
+
+horizontal_bars <- df_only_mandf %>%
+  ggplot() +
+  aes(x=reorder(name, -pct_diff), y=pct_diff, fill=diff_size) +
+  geom_bar(stat="identity") +
+  coord_flip() +
+  labs(
+    title = str_wrap("% Difference in occurrences of names between females and males", width=65),
+    subtitle = str_wrap("Positive values show a name is more common among women, negative show a name is more common among men.", width=65),
+    x = "Names",
+    y = "Percent difference in occurrences of name by gender",
+    fill = str_wrap("Total Name Count", width=15)
+  ) +
+  theme(
+    axis.text.y = element_text(size = 10, angle=0)
+  )
+
+ggsave("column_chart_1.png", 
+       plot = vertical_cols, 
+       width = 8, 
+       height = 6, 
+       units = "in", 
+       dpi = 300)
+
+ggsave("bar_chart_1.png", 
+       plot = horizontal_bars, 
+       width = 7, 
+       height = 8, 
+       units = "in", 
+       dpi = 300)
